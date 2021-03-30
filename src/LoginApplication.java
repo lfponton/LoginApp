7b55891365f1fce23c0641800972339ce2ff54dc
Luis <lfponton@gmail.com>
2021-03-30 11:52:35 +0200

    Initial commit

---
+++ src/LoginApplication.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import core.ModelFactory;
+import core.ViewHandler;
+import core.ViewModelFactory;
+
+public class LoginApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // creating the core classes, that are needed.
+        // If I had a client, I would create it here too, and hand over to the ModelFactory
+
+        ModelFactory mf = new ModelFactory();
+        ViewModelFactory vmf = new ViewModelFactory(mf);
+        ViewHandler vh = new ViewHandler(vmf);
+        vh.start();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        // This method is called, when the application is shut down
+        System.out.println("Shutting down.");
+    }
+}
